ecc8c45341902d6c4ed0f39a88ca4b727e1c5cc3
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-09-17 10:32:44 +0530

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "hc-430e7eefb27e4cc18e75a35037a"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
