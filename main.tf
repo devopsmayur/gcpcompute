9e353088fa1026b998fd06a0de455edb73707280
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-09-18 22:43:35 +0530

    Update and rename compute.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+# Define provider configuration
+provider "google" {
+  project     = "hc-a0411e550991411898ee2ce2c2e"
+  region      = "us-central1"
+}
+
+# Reference the network from the Network Workspace
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = "devopsmayur"
+    workspace    = "gcpnetwork"
+  }
+}
+
+# Create a Compute Engine instance
+resource "google_compute_instance" "my_instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = data.terraform_remote_state.network.outputs.network_self_link
+    subnetwork = data.terraform_remote_state.network.outputs.subnet_self_link
+  }
+}
+
+# Output the instance public IP
+output "instance_public_ip" {
+  value = google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip
+}
