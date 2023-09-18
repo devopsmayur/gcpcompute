d392982e95f9d87babe065c6e4b4c14109220d64
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-09-18 22:26:58 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -22,10 +22,4 @@ resource "google_compute_instance" "vm_instance" {
       image = "debian-cloud/debian-11"
     }
   }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
 }
